759e0a4a7f19b78979a9aa4b79b9b39e1494462b
Leonhardt Schwarz <if12b076@technikum-wien.at>
2014-03-14 16:11:19 +0100

    Postgres Test

---
+++ DB Scripts/2CreateTable.sql
@@ -0,0 +1,8 @@
+﻿create table test
+(
+	pk_test serial primary key,
+	test_data text
+);
+
+insert into test(test_data) values ('blub');
+Insert into test(test_data) VALUES ('blubasdl');
--- Server/Program.cs
+++ Server/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
 
 namespace Server
 {
@@ -10,6 +11,20 @@ namespace Server
 	{
 		static void Main(string[] args)
 		{
+			NpgsqlConnection dbConn = new NpgsqlConnection("Server=127.0.0.1;Port=5432;User Id=sweadmin;Password=swe;Database=swedb;");
+			dbConn.Open();
+			string sql = "SELECT pk_test, test_data FROM test";
+			NpgsqlCommand dbCommand = new NpgsqlCommand(sql, dbConn);
+			NpgsqlDataReader dbDataReader = dbCommand.ExecuteReader();
+			while(dbDataReader.Read())
+			{
+				String id = dbDataReader[0].ToString();
+				String data = dbDataReader[1].ToString(); 
+				Console.WriteLine(id + " | " + data);
+			}
+			dbConn.Close();
+			Console.ReadKey();
+
 		}
 	}
 }
