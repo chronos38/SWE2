635cbfde8f5cb14880bc16818d4f94a242137712
David Wolf <d.wolf@live.at>
2014-03-15 13:56:58 +0100

    Branch auf 'dev' gewechselt.

--- DB Scripts/2CreateTable.sql
+++ DB Scripts/2CreateTable.sql
@@ -1,8 +1,20 @@
-﻿create table test
-(
-	pk_test serial primary key,
-	test_data text
+﻿create table Address (
+	ID serial primary key,
+	Street text not null,
+	StreetNumber text not null,
+	PostalCode integer not null,
+	City text not null
 );
 
-insert into test(test_data) values ('blub');
-Insert into test(test_data) VALUES ('blubasdl');
+create table Contact (
+	ID serial primary key,
+	Name text,
+	Title text null,
+	Forename text null,
+	Surname text null,
+	Suffix text null,
+	BirthDate text null,
+	Address serial reference Address (ID),
+	BillingAddress serial reference Address(ID),
+	DeliveryAddress serial reference Address(ID)
+);
