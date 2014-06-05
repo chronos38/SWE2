f1bc3d32392659e34f6c30e4a94b7d22305c79ac
Leonhardt Schwarz <if12b076@technikum-wien.at>
2014-06-05 21:29:03 +0200

    Facade Tests

---
+++ Tests/FacadeTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Server.BusinessLayer;
+using Server.RPC;
+using Tests.Server.Commands;
+using Tests.Server.RPC;
+using DataTransfer;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Tests
+{
+	[TestClass]
+	public class FacadeTest
+	{
+		[TestInitialize]
+		public void Setup()
+		{
+			CommandDictionary.Instance.RegisterCommand("CommandTest", new CommandTest());
+		}
+
+		[TestMethod]
+		public void FacadeHandleConnectionExecutesExistingCommand()
+		{
+			MockHttpConnection con = new MockHttpConnection();
+			con.CommandName = "CommandTest";
+
+			Facade fc = new Facade();
+			fc.HandleConnection(con);
+			RPResult res = Deserialize(con.ResponseData);
+			Assert.AreEqual(12345, res.count);
+		}
+
+		[TestMethod]
+		public void FacadeHandleConnectionError500OnNonExistingCommand()
+		{
+			MockHttpConnection con = new MockHttpConnection();
+			con.CommandName = "CommandDoesNotExist";
+
+			Facade fc = new Facade();
+			fc.HandleConnection(con);
+			Assert.AreEqual(500, con.StatusCode);
+		}
+
+		private RPResult Deserialize(string res)
+		{
+			XmlSerializer RPResultSerializer = new XmlSerializer(typeof(RPResult));
+			StringReader reader = new StringReader(res);
+			RPResult ret = (RPResult)RPResultSerializer.Deserialize(reader);
+			return ret;
+		}
+	}
+}
---
+++ Tests/Server/RPC/MockHttpConnection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Server.RPC;
+using System.IO;
+using DataTransfer;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Tests.Server.RPC
+{
+	class MockHttpConnection : IHttpConnection
+	{
+		XmlSerializer _RPCallSerializer = new XmlSerializer(typeof(RPCall));
+		string _response = null;
+		int _statusCode;
+		string _commandName = "CommandTest";
+
+		public string CommandName
+		{
+			get { return _commandName; }
+			set { _commandName = value; }
+		}
+		public string ResponseData
+		{
+			get { return _response; }
+		}
+
+		public int StatusCode
+		{
+			get { return _statusCode; }
+		}
+
+		public StreamReader GetClientData()
+		{
+			RPCall call = new RPCall(_commandName);
+			MemoryStream stream = new MemoryStream();
+			_RPCallSerializer.Serialize(stream, call);
+			stream.Flush();
+			stream.Seek(0, SeekOrigin.Begin);
+			return new StreamReader(stream);
+		}
+
+		public void SendReponseData(byte[] response)
+		{
+			_response = Encoding.UTF8.GetString(response);
+			_statusCode = 200;
+		}
+
+		public void SendErrorCode(int statusCode)
+		{
+			_statusCode = statusCode;
+		}
+	}
+}
